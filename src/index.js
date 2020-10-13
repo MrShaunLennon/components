d770a034e4cc74d8ed47dccccc00ad612943705a
Shaun Lennon <shaunlennon1992@gmail.com>
2020-10-13 14:00:15 +0100

    Added commentText props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail author="Sam" />
-      <CommentDetail author="Alex" />
-      <CommentDetail author="James" />
+      <CommentDetail author="Sam" timeAgo="Today at 21:39pm" commentText="Nice blog post!" />
+      <CommentDetail author="Alex" timeAgo="Today at 15:35pm" commentText="Great job!" />
+      <CommentDetail author="James" timeAgo="Yesterday at 17:00pm" commentText="Amazing read!" />
     </div>
   );
 };
