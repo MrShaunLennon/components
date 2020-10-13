d80abbf976cead7480151220fee523fcac5f8fec
Shaun Lennon <shaunlennon1992@gmail.com>
2020-10-13 11:21:20 +0100

    Created component and appended to root

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hi there!</div>
+};
+
+ReactDOM.render(
+  <div><App /></div>,
+  document.querySelector('#root'));
